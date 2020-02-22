383419febf2bee629f73c01d25c4a3dc8e9c0b70
undind <flyker777@yandex.ru>
2020-02-22 18:33:42 +0300

    Added new packages in project

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './assets/styles/index.scss';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import "antd/dist/antd.css";
+import "./assets/styles/index.scss";
+
+import App from "./App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
