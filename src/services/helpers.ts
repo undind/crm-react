cbbc158198ca5f40811ea1733be88e2d6530d97e
undind <flyker777@yandex.ru>
2020-03-29 18:06:30 +0300

    Rewrite helpers

--- src/services/helpers.ts
+++ src/services/helpers.ts
@@ -6,50 +6,38 @@ interface InputsProps {
 }
 
 export const generateFormFields = (type: string): Array<InputsProps> => {
+    const loginFormArrayes = [
+        {
+            iconType: 'user',
+            type: 'text',
+            placeholder: 'Username',
+            name: 'username'
+        },
+        {
+            iconType: 'mail',
+            type: 'email',
+            placeholder: 'E-mail',
+            name: 'email'
+        },
+        {
+            iconType: 'lock',
+            type: 'password',
+            placeholder: 'Password',
+            name: 'password'
+        },
+        {
+            iconType: 'lock',
+            type: 'password',
+            placeholder: 'Confirm password',
+            name: 'password_2'
+        }
+    ]
     switch (type) {
         case 'signin':
-            return [
-                {
-                    iconType: 'user',
-                    type: 'text',
-                    placeholder: 'Username',
-                    name: 'username'
-                },
-                {
-                    iconType: 'lock',
-                    type: 'password',
-                    placeholder: 'Password',
-                    name: 'password'
-                }
-            ];
+            return [loginFormArrayes[0], loginFormArrayes[2]];
 
         case 'signup':
-            return [
-                {
-                    iconType: 'user',
-                    type: 'text',
-                    placeholder: 'Username',
-                    name: 'username'
-                },
-                {
-                    iconType: 'mail',
-                    type: 'email',
-                    placeholder: 'E-mail',
-                    name: 'email'
-                },
-                {
-                    iconType: 'lock',
-                    type: 'password',
-                    placeholder: 'Password',
-                    name: 'password'
-                },
-                {
-                    iconType: 'lock',
-                    type: 'password',
-                    placeholder: 'Confirm password',
-                    name: 'password_2'
-                }
-            ];
+            return loginFormArrayes;
 
         default:
             return [];
